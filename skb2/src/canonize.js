f4be22708bbb3cb2934d17aeaf32cc13e270fa56
AndreyTurubchuk <antur1977@rambler.ru>
2016-11-16 21:59:14 +0300

    1. Удалил ненужные комментарии

--- skb2/src/canonize.js
+++ skb2/src/canonize.js
@@ -1,7 +1,4 @@
 export default function canonize(url) {
-//  const re = new RegExp('@?(https?:)?(\/\/)?((Авааз|Badoo|Facebook|Instagram|Google|LinkedIn|Pinterest|Renren|Twitter|' +
-  //  'ВКонтакте|Одноклассники|telegram|vk|vkontakte|ok)[^\/]*\/)?([a-zA-Z0-9]*)', 'i');
-
   const re = new RegExp('@?(https?:)?(\/\/)?(([a-z])[^\/]*\/)?([a-zA-Z0-9]*)', 'i');
   const username = url.match(re)[5];
   return '@' + username;
